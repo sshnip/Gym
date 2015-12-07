eb4b79f4baf3a8db434f6d6da7b885a3c14ce787
Shnip Siarhei <s.shnip@gmail.com>
2015-12-07 15:37:18 +0300

    Initial commit

---
+++ GymTests/GymTests.swift
@@ -0,0 +1,36 @@
+//
+//  GymTests.swift
+//  GymTests
+//
+//  Created by Sergey Shnip on 12/7/15.
+//  Copyright © 2015 Sergey Shnip. All rights reserved.
+//
+
+import XCTest
+@testable import Gym
+
+class GymTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
